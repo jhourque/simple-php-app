50ebdfd35e96e0f24faa1417a096b3cc9beb0776
Jerome HOURQUEBIE <jerome.hourquebie@d2-si.eu>
2017-12-18 13:16:30 +0000

    fix travis

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,9 @@
 cd terraform
+mkdir -p ~/.ssh
+echo $id_rsa > ~/.ssh/id_rsa.42.pub
 terraform init
-terraform taint 'aws_instance.front.0'
-terraform apply -auto-approve
-terraform taint 'aws_instance.front.1'
-terraform apply -auto-approve
+#terraform taint 'aws_instance.front.0'
+#terraform apply -auto-approve
+#terraform taint 'aws_instance.front.1'
+#terraform apply -auto-approve
 
